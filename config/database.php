e7ea4c764ea4b465c420cecb9ed19c09fb3b4985
mailm4n-fit <mailm4n@gmail.com>
2026-07-22 18:38:16 +0300

    database.php

---
+++ config/database.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'host' => 'localhost',
+
+    'database' => 'fitmode_crm',
+
+    'username' => 'UTILIZATOR_DB',
+
+    'password' => 'PAROLA_DB',
+
+    'charset' => 'utf8mb4'
+
+];
