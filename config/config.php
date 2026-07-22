dd2d2db1256cd359f40c5bc706366c7428be012d
mailm4n-fit <mailm4n@gmail.com>
2026-07-22 18:03:12 +0300

    config.php

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('APP_NAME', 'FitMode CRM');
+
+define('APP_URL', 'https://crm.fitmode.ro');
+
+define('APP_VERSION', '1.0.0');
+
+date_default_timezone_set('Europe/Bucharest');
+
+session_start();
